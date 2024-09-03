15e70312b31bf7c7792d6cba333e200717f46700
BasobPragyan <pragyan95prabodhbanerjee@gmail.com>
2024-09-03 21:58:22 +0530

    Python operators 3

--- activity1.py
+++ activity1.py
@@ -1,1 +1,15 @@
-print("Hello Manchester")
+a=int(input("Enter number"))
+b=int(input("Enter second number"))
+a is b
+print("a is b",a is b)
+if (a is b) :
+    print("Both are equal")
+else:
+    print("Both are not equal")
+
+a="Manchester United"
+b="Old Tradford"
+if (b in a):
+    print("b is in a",b in a)
+else:
+    print("It is not in a",b in a)
---
+++ activity2.py
@@ -0,0 +1,8 @@
+a=4
+b=5
+print(a&b)
+print(a|b)
+print(a*b)
+print(~a)
+print("Left shift by 1",a<<1)
+print("Right shift by 1",a>>1)
